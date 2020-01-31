da62481337b40363cad8ed4d35b43e1601057754
jaehun-shim-dev <jaehun.shim.dev@gmail.com>
2020-01-31 18:08:16 +0900

    testing DtoController

--- src/test/java/com/faunaface/webservice/web/WebRestControllerTest.java
+++ src/test/java/com/faunaface/webservice/web/WebRestControllerTest.java
@@ -1,16 +1,20 @@
 package com.faunaface.webservice.web;
 
+import com.faunaface.webservice.web.dto.HelloResponseDto;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.test.web.servlet.ResultMatcher;
 
+import org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 
 @RunWith(SpringRunner.class)
@@ -21,10 +25,29 @@ public class WebRestControllerTest {
     private MockMvc mvc;
 
     @Test
-    public void HelloTest() throws Exception {
+    public void helloTest() throws Exception {
         String hello = "hello";
 
-            mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+            mvc.perform(get("/hello"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(hello));
+
+    }
+
+    @Test
+    public void helloDtoTest() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+            .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", Matchers.is(name)))
+                .andExpect(jsonPath("$.amount", Matchers.is(amount)));
+
+
 
     }
 
