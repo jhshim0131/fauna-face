375978471a4494a5a7badf7a5948d3ea10fcca3c
jaehun-shim-dev <jaehun.shim.dev@gmail.com>
2020-02-08 00:34:05 +0900

    Creating H2 Datasource configuration, Creating Update API

--- src/main/java/com/faunaface/webservice/domain/service/PostsService.java
+++ src/main/java/com/faunaface/webservice/domain/service/PostsService.java
@@ -2,7 +2,9 @@ package com.faunaface.webservice.domain.service;
 
 import com.faunaface.webservice.domain.posts.Posts;
 import com.faunaface.webservice.domain.posts.PostsRepository;
+import com.faunaface.webservice.web.dto.PostsResponseDto;
 import com.faunaface.webservice.web.dto.PostsSaveRequestDto;
+import com.faunaface.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,5 +20,24 @@ public class PostsService {
         return postsRepository.save(requestDto.toEntity()).getId();
 
     }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("The User is " +
+                "not existed id="+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("The User is " +
+                        "not existed id="+id));
+
+        return new PostsResponseDto(entity);
+    }
+
 
 }
--- src/main/java/com/faunaface/webservice/web/PostsApiController.java
+++ src/main/java/com/faunaface/webservice/web/PostsApiController.java
@@ -2,7 +2,9 @@ package com.faunaface.webservice.web;
 
 import com.faunaface.webservice.domain.service.PostsService;
 import com.faunaface.webservice.web.dto.PostsSaveRequestDto;
+import com.faunaface.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,9 +17,16 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
+    //Create API
     @PostMapping("/api/v1/posts") //Putmapping is not appropriate to save posts
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    //Update API
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
     }
 
 
